c2c55ca7743ec1b217b380e8f8d9c805825e023b
Pattharapong Chotchuang <roy@Roys-MBA.local>
2019-04-07 00:35:29 +0700

    dummy index page

--- router.js
+++ router.js
@@ -6,13 +6,13 @@ const   express = require('express'),
         moment = require('moment');
 
 router.all('/', (req, res) => {
-    mysql.connect('CREATE TABLE IF NOT EXISTS tasks (task_id INT AUTO_INCREMENT,title VARCHAR(255) NOT NULL,PRIMARY KEY (task_id));')
-        .then((resp)=>{
-            console.log(resp);
-        })
-        .catch((err)=>{
-            console.log(err);
-        });
+    // mysql.connect('CREATE TABLE IF NOT EXISTS tasks (task_id INT AUTO_INCREMENT,title VARCHAR(255) NOT NULL,PRIMARY KEY (task_id));')
+    //     .then((resp)=>{
+    //         console.log(resp);
+    //     })
+    //     .catch((err)=>{
+    //         console.log(err);
+    //     });
     res.render('index');
 })
 module.exports = router;
