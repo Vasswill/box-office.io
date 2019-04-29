f9f487ee8c36af1c1a5659261ab6d26ec186fc03
mark <pawinchio@yahoo.com>
2019-04-29 20:07:19 +0700

    update schedule.ejs by pawi.c

--- public/scripts/schedule.js
+++ public/scripts/schedule.js
@@ -5,24 +5,49 @@ function showmovie(data) {
             
             $("#Movie").append('<tr class="default-mouse"><th class="text-white pl-3 movieTable" scope="col">'+value.MovieName+'</th></tr>');
         });
-        // data.forEach((value, key) => {
-        // var tableRowappend = '<tr id="Th'+Thcount+'" class="default-mouse" ><th onclick="editTh('+Thcount+')" class="text-white pl-3" scope="col">'+value.Name+'</th>'
-        
-        // if(Thcount>0) tableRowappend += '<th class="text-white" onclick="removeTh('+Thcount+')" scope="col">X</th>';
-        // else tableRowappend += '<th onclick="editTh(0)"></th>';
-        // tableRowappend += '</tr>';
-        // Thcount++;
-        // $("#MyTableTr").append(tableRowappend);
+        console.log(data)
+    });
+    
+}
+function showbranch(data) {
+    var payload = { table:"branch" };
+    $.post('/fetchData',payload,(data)=>{
+        data.forEach((value,key)=>{
+            
+            $("#Branch").append('<tr class="default-mouse"><th class="text-white pl-3 branchTable" scope="col">'+value.BranchNo+'</th></tr>');
+        });
         console.log(data)
     });
     
 }
 
-function test(){
+function showTheater(data) {
+    var payload = { table:"theater" };
+    $.post('/fetchData',payload,(data)=>{
+        data.forEach((value,key)=>{
+            if(branch.temp==value.BranchNo)
+            $("#theater").append('<tr class="default-mouse"><th class="text-white pl-3 branchTable" scope="col">'+value.TheaterCode+'</th></tr>');
+        });
+        console.log(data)
+    });
+    
+}
+
+
+function movie(){
     console.log(this.innerHTML);
     var temp = this.innerHTML;
 }
+function branch(){
+    console.log(this.innerHTML);
+    var temp = this.innerHTML;
+    
+}
 
 showmovie();
-$(document).on('click',".movieTable",test);
+showbranch();
+$(document).on('click',".movieTable",movie);
+$(document).on('click',".branchTable",branch);
+showTheater();
+
 
